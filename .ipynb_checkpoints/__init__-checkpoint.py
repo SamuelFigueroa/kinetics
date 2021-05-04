6464aae2a900109c479fba58fd7316caed8cf71d
Samuel Figueroa-Lazu <sammylazu@gmail.com>
2021-05-04 10:58:25 -0500

    Ignore directories that were previously committed

--- .ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-# from .FSQ import compFTOfSpectrum, compSortedEigenSet, compNumEigenVectors, compSEE
-# from .initial_rates import compInitialRate, compLeastSquaresRateLaw
-# from .json_definitions import readCalibrationData, readKineticData
